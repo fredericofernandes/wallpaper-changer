8ab2018d0b358b186d27f9ee1f1280f3ec2f04e9
Frederico Fernandes <frederico.mp.fernandes@gmail.com>
2018-02-17 22:08:25 +0000

    Initial commit

---
+++ wallpaper-changer.py
@@ -0,0 +1,13 @@
+import os
+import argparse
+
+def set_wallpaper(file):
+    os.system("sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db \"update data set value = '" + file + "'\" && killall Dock")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('wallpaper', help='wallpaper image file')
+    args = parser.parse_args()
+    set_wallpaper(args.wallpaper)
+
+main()
